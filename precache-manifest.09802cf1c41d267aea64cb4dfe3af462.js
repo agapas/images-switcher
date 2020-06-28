6d1a5f58d0f86e149971d7ec3f331d5ed039b608
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2020-06-28 12:39:21 +0100

    Updates

---
+++ precache-manifest.09802cf1c41d267aea64cb4dfe3af462.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe8e866f13c6f8731aa5bdd3503185c",
+    "url": "/images-switcher/index.html"
+  },
+  {
+    "revision": "ded8f3506e4cb6607bef",
+    "url": "/images-switcher/static/css/main.40a61016.chunk.css"
+  },
+  {
+    "revision": "a7e406781130a17b3bca",
+    "url": "/images-switcher/static/js/2.a44c812e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/images-switcher/static/js/2.a44c812e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded8f3506e4cb6607bef",
+    "url": "/images-switcher/static/js/main.d8b29825.chunk.js"
+  },
+  {
+    "revision": "0043ef818f8d007b2cdb",
+    "url": "/images-switcher/static/js/runtime-main.b6e17f69.js"
+  },
+  {
+    "revision": "1e921e25c87f42fdd212496b52514225",
+    "url": "/images-switcher/static/media/1.1e921e25.jpg"
+  },
+  {
+    "revision": "70eeeb456ae7897649032e2ff721a05e",
+    "url": "/images-switcher/static/media/1.70eeeb45.jpg"
+  },
+  {
+    "revision": "1c28f435e40c46b8b85c79f9b6d82e28",
+    "url": "/images-switcher/static/media/2.1c28f435.jpg"
+  },
+  {
+    "revision": "75eb84307af0b74a8a8d603aaf5c19d8",
+    "url": "/images-switcher/static/media/2.75eb8430.jpg"
+  },
+  {
+    "revision": "6b16d48006a1eaa233f7dae3efedbfd8",
+    "url": "/images-switcher/static/media/3.6b16d480.jpg"
+  },
+  {
+    "revision": "a3ad066f063e708d02fc129695c76082",
+    "url": "/images-switcher/static/media/3.a3ad066f.jpg"
+  },
+  {
+    "revision": "78bec073397d54a68f80879dd87fd45c",
+    "url": "/images-switcher/static/media/4.78bec073.jpg"
+  },
+  {
+    "revision": "d2a72859381ae5306ab982c8b827dcae",
+    "url": "/images-switcher/static/media/4.d2a72859.jpg"
+  },
+  {
+    "revision": "43825f8091e76d8641ce707c40ec665d",
+    "url": "/images-switcher/static/media/5.43825f80.jpg"
+  },
+  {
+    "revision": "99d0d97b61b8060747a401eaf974cdc8",
+    "url": "/images-switcher/static/media/5.99d0d97b.jpg"
+  },
+  {
+    "revision": "7ff0b466502725cfc120b2637d0952c9",
+    "url": "/images-switcher/static/media/6.7ff0b466.jpg"
+  },
+  {
+    "revision": "c41e052a1f18c55ef041d9fd6c80b18b",
+    "url": "/images-switcher/static/media/6.c41e052a.jpg"
+  }
+]);
